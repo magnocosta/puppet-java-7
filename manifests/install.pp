35cbf48cb9492f2a1579779e9b338236b9623cc4
Magno Costa <magnocosta.br@gmail.com>
2014-10-16 18:42:44 -0300

    Adicionado o projeto no github.

---
+++ manifests/install.pp
@@ -0,0 +1,23 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+class java::install {
+
+    exec { "add-repository-apt":
+        command  => "add-apt-repository ppa:webupd8team/java",
+        unless   => "grep -Ec java /etc/apt/sources.list.d/*.list"
+        required => Class["common::basic"]
+    }
+
+    exec { "apt-update-java":
+        command => "/usr/bin/apt-get update",
+        require => Exec["add-repository-apt"]
+    }
+
+    package { "oracle-java7-installer":
+        ensure => installed,
+        required => Exec["apt-update-java"]
+    }
+
+}
+
