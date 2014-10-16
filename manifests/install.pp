c29bc228fe9928fe076633c6d0832843b9926a71
Magno Costa <magnocosta.br@gmail.com>
2014-10-16 19:15:32 -0300

    Corrigido erro na instalacao do repositorio.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class java::install {
     exec { "add-repository-apt":
         command  => "add-apt-repository ppa:webupd8team/java",
         unless   => "grep -Ec java /etc/apt/sources.list.d/*.list",
-        required => Class["common::basic"]
+        require => Class["common::basic"]
     }
 
     exec { "apt-update-java":
@@ -15,8 +15,8 @@ class java::install {
     }
 
     package { "oracle-java7-installer":
-        ensure => installed,
-        required => Exec["apt-update-java"]
+        ensure  => installed,
+        require => Exec["apt-update-java"]
     }
 
 }
